a85ec07355c881f6835f3a085e1269618ca2ec6d
Azat Roku <roku@azat.dev>
2026-05-15 18:14:53 -0400

    Working on animatable

---
+++ app/styles/components/_ord_scrollGroup.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "_ord_scrollGroup": {
+        "translation": [210, 410],
+        "clippingRect": [0, 0, 1650, 700],
+        "Rectangle": {
+            "color": "#000000",
+            "&#container": {
+                "color": "#000000",
+                "height": 700,
+                "width": 1650,
+                "translation": [0, 0],
+                "Rectangle": {
+                    "&#first": {
+                        "color": "#FF0000",
+                        "height": 300,
+                        "width": 1650
+                    },
+                    "&#second": {
+                        "color": "#eeff00",
+                        "height": 300,
+                        "width": 1650
+                    },
+                    "&#third": {
+                        "color": "#e6c5c5",
+                        "height": 300,
+                        "width": 1650
+                    },
+                    "&#fourth": {
+                        "color": "#ff00dd",
+                        "height": 300,
+                        "width": 1650
+                    }
+                }
+            }
+        }
+    }
+};
--- app/styles/components/index.js
+++ app/styles/components/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     ...require("./_ord_flexList"),
+    ...require("./_ord_scrollGroup"),
     ...require("./ButtonItem"),
     ...require("./CarouselItem"),
     ...require("./DynamicItem"),
