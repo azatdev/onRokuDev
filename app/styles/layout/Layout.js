368e0c841066976979afa6412c069e728a6d021e
Azat Roku <roku@azat.dev>
2026-05-19 16:00:36 -0400

    Layout init.

---
+++ app/styles/layout/Layout.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "Layout": {
+        "Background": {
+            "visible": true
+        },
+        "_ord_scrollGroup": {
+            height: 700,
+            width: 1650
+        }
+    }
+};
--- app/styles/views/App.js
+++ app/styles/views/App.js
@@ -2,13 +2,6 @@ module.exports = {
     "App": {
         "backgroundColor": "#000000ff",
         "backgroundUri": "",
-        "Background": {
-            "visible": true
-        },
-        "_ord_scrollGroup": {
-            height: 700,
-            width: 1650
-        },
         "CTAs": {
             color: "#bad7fc00",
             translation: [150, 210]
