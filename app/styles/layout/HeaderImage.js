54c9317fa118ff6f51fb364cdee1f7f49f1ba68f
Azat Roku <roku@azat.dev>
2026-06-04 23:28:09 -0400

    Header image implementation.

---
+++ app/styles/layout/HeaderImage.js
@@ -0,0 +1,44 @@
+const mixins = require("../mixins");
+const dimensions = mixins["Dimensions"]
+const palette = mixins["Palette"]
+const viewport = dimensions["viewport"]
+
+module.exports = {
+    "HeaderImage": {
+        height: 561,
+        opacity: 0,
+        translation: [921, 60],
+        width: 999,
+        "MaskGroup": {
+            "maskuri": "pkg:/static/images/gradients/header-image-mask-test.webp"
+        },
+        "Div": {
+            "&#headerImageGradients": {
+                visible: false,
+                "Poster": {
+                    "&#leftGradient": {
+                        "blendColor": "#000000",
+                        "height": 561,
+                        "translation": [0, 0],
+                        "uri": "pkg:/static/images/gradients/header-image-left-gradient.webp",
+                        "width": 179
+                    },
+                    "&#topGradient": {
+                        "blendColor": "#000000",
+                        "height": 171,
+                        "translation": [0, 0],
+                        "uri": "pkg:/static/images/gradients/header-image-top-gradient.webp",
+                        "width": 999
+                    },
+                    "&#bottomGradient": {
+                        "blendColor": "#000000",
+                        "height": 294,
+                        "translation": [0, 561 - 294],
+                        "uri": "pkg:/static/images/gradients/header-image-bottom-gradient.webp",
+                        "width": 999
+                    },
+                }
+            }
+        }
+    }
+};
--- app/styles/layout/index.js
+++ app/styles/layout/index.js
@@ -3,6 +3,7 @@ module.exports = {
     ...require("./Carousel"),
     ...require("./Delimiter"),
     ...require("./Header"),
+    ...require("./HeaderImage"),
     ...require("./Hero"),
     ...require("./CatalogHero"),
     ...require("./Layout"),
