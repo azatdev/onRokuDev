a5bc54691ff6d0d25940a898586acc86576e7e75
Azat Roku <roku@azat.dev>
2026-08-10 22:13:32 -0400

    Renaming static to assets folder.

--- app/styles/mixins/Assets.js
+++ app/styles/mixins/Assets.js
@@ -1,43 +1,43 @@
 const gradients = {
-    "bottomGradient": "pkg:/static/images/gradients/bottom-gradient.webp",
-    "contentLeftGradient": "pkg:/static/images/gradients/content-left-gradient.webp",
-    "contentTopGradient": "pkg:/static/images/gradients/content-top-gradient.webp",
-    "cornerGradient": "pkg:/static/images/gradients/corner-gradient.webp",
-    "headerBottom":"pkg:/static/images/gradients/header-image-bottom-gradient.webp",
-    "headerLeft": "pkg:/static/images/gradients/header-image-left-gradient.webp",
-    "headerTop": "pkg:/static/images/gradients/header-image-top-gradient.webp",
-    "headerMask": "pkg:/static/images/gradients/header-image-mask.webp",
-    "sidebarGradient": "pkg:/static/images/gradients/sidebar-gradient.webp",
-    "topbarMask": "pkg:/static/images/gradients/topbar-mask-white.webp"
+    "bottomGradient": "pkg:/assets/images/gradients/bottom-gradient.webp",
+    "contentLeftGradient": "pkg:/assets/images/gradients/content-left-gradient.webp",
+    "contentTopGradient": "pkg:/assets/images/gradients/content-top-gradient.webp",
+    "cornerGradient": "pkg:/assets/images/gradients/corner-gradient.webp",
+    "headerBottom":"pkg:/assets/images/gradients/header-image-bottom-gradient.webp",
+    "headerLeft": "pkg:/assets/images/gradients/header-image-left-gradient.webp",
+    "headerTop": "pkg:/assets/images/gradients/header-image-top-gradient.webp",
+    "headerMask": "pkg:/assets/images/gradients/header-image-mask.webp",
+    "sidebarGradient": "pkg:/assets/images/gradients/sidebar-gradient.webp",
+    "topbarMask": "pkg:/assets/images/gradients/topbar-mask-white.webp"
 }
 
 const ninePatches = {
-    "highlightBottom18": "pkg:/static/images/$$RES$$/9patches/highlight-bottom-18px.9.png",
-    "highlightTop18": "pkg:/static/images/$$RES$$/9patches/highlight-top-18px.9.png",
-    "fill9": "pkg:/static/images/$$RES$$/9patches/fill-9px.9.png",
-    "outline918": "pkg:/static/images/$$RES$$/9patches/9px-outline-18px.9.png",
-    "rounded6": "pkg:/static/images/$$RES$$/9patches/fill-6px.9.png",
-    "rounded18": "pkg:/static/images/$$RES$$/9patches/fill-18px.9.png",
-    "tabBottom18": "pkg:/static/images/$$RES$$/9patches/tab-bottom-18px.9.png",
-    "tabTop18": "pkg:/static/images/$$RES$$/9patches/tab-top-18px.9.png"
+    "highlightBottom18": "pkg:/assets/images/$$RES$$/9patches/highlight-bottom-18px.9.png",
+    "highlightTop18": "pkg:/assets/images/$$RES$$/9patches/highlight-top-18px.9.png",
+    "fill9": "pkg:/assets/images/$$RES$$/9patches/fill-9px.9.png",
+    "outline918": "pkg:/assets/images/$$RES$$/9patches/9px-outline-18px.9.png",
+    "rounded6": "pkg:/assets/images/$$RES$$/9patches/fill-6px.9.png",
+    "rounded18": "pkg:/assets/images/$$RES$$/9patches/fill-18px.9.png",
+    "tabBottom18": "pkg:/assets/images/$$RES$$/9patches/tab-bottom-18px.9.png",
+    "tabTop18": "pkg:/assets/images/$$RES$$/9patches/tab-top-18px.9.png"
 }
 
 const ord = {
-    "clippingRect": "pkg:/static/images/thinner_rect.9.png",
-    "whiteBlock": "pkg:/static/images/white-block.webp"
+    "clippingRect": "pkg:/assets/images/thinner_rect.9.png",
+    "whiteBlock": "pkg:/assets/images/white-block.webp"
 }
 
 const player = {
-    "playHead": "pkg:/static/images/playhead.webp"
+    "playHead": "pkg:/assets/images/playhead.webp"
 }
 
 module.exports = {
     "Assets": {
         "fonts": {
-            "icons": "pkg:/static/fonts/azatroku-icons.ttf",
-            "quanticoBold": "pkg:/static/fonts/Quantico-Bold.ttf",
-            "quanticoBoldItalic": "pkg:/static/fonts/Quantico-BoldItalic.ttf",
-            "quanticoRegular": "pkg:/static/fonts/Quantico-Regular.ttf"
+            "icons": "pkg:/assets/fonts/azatroku-icons.ttf",
+            "quanticoBold": "pkg:/assets/fonts/Quantico-Bold.ttf",
+            "quanticoBoldItalic": "pkg:/assets/fonts/Quantico-BoldItalic.ttf",
+            "quanticoRegular": "pkg:/assets/fonts/Quantico-Regular.ttf"
         },
         ...gradients,
         ...ninePatches,
